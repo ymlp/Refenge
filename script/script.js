7d43c313cfb8939b1d1196197a0efa85c78c3cc2
Yuri Lago Philomeno <56745452+ymlp@users.noreply.github.com>
2022-10-05 16:53:21 -0300

    Logica na auto seleção das opções do cabeçalho em andamento

--- script/script.js
+++ script/script.js
@@ -1,7 +1,29 @@
 var btn = document.querySelector(".btn");
+var opEstoque = document.querySelector(".OpsCabe0");
+var opEntrada = document.querySelector(".OpsCabe1");
+var opSaida = document.querySelector(".OpsCabe2");
 
+
+if()
 btn.addEventListener("click", (e)=>{
     if(true){
-    open("teste.html","_self");
+    open("estoque.html","_self");
     }
 });
+
+var paginaAtual = document.querySelector(title);
+
+switch(paginaAtual){
+    case "Estoque":
+        opEstoque.style.backgroundColor = "#494981";
+        opEstoque.style.color = "#fff";
+        break;
+    case "Entrada":
+        opEstoque.style.backgroundColor = "#494981";
+        opEstoque.style.color = "#fff";
+        break;
+    case "Saída":
+        opEstoque.style.backgroundColor = "#494981";
+        opEstoque.style.color = "#fff";
+        break;
+}
