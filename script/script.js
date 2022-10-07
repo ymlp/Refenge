f8bcfb6c5377363e167e8f13d8d6f9f553620be1
Yuri Lago Philomeno <56745452+ymlp@users.noreply.github.com>
2022-10-07 16:54:17 -0300

    Parte visual da tela estoque completa, fora o display interno da tabela

--- script/script.js
+++ script/script.js
@@ -1,22 +1,23 @@
 var btn = document.querySelector(".btn");
-var opEstoque = document.querySelector(".OpsCabe0");
-var opEntrada = document.querySelector(".OpsCabe1");
-var opSaida = document.querySelector(".OpsCabe2");
+var opEstoque = document.querySelector(".opsCabe1");
+var opEntrada = document.querySelector(".opsCabe2");
+var opSaida = document.querySelector(".opsCabe3");
 
-
-if()
-btn.addEventListener("click", (e)=>{
-    if(true){
-    open("estoque.html","_self");
-    }
-});
-
-var paginaAtual = document.querySelector(title);
+var paginaAtual = document.title;
+console.log(paginaAtual);
+if(paginaAtual == "Login"){
+    btn.addEventListener("click", (e)=>{
+        if(true){
+            open("estoque.html","_self");
+        }
+    });
+};
 
 switch(paginaAtual){
     case "Estoque":
         opEstoque.style.backgroundColor = "#494981";
         opEstoque.style.color = "#fff";
+        console.log("Estoque");
         break;
     case "Entrada":
         opEstoque.style.backgroundColor = "#494981";
@@ -26,4 +27,52 @@ switch(paginaAtual){
         opEstoque.style.backgroundColor = "#494981";
         opEstoque.style.color = "#fff";
         break;
-}
+};
+
+var opTabela1 = document.querySelector(".op1");
+var opTabela2 = document.querySelector(".op2");
+var opTabela3 = document.querySelector(".op3");
+var opTabela4 = document.querySelector(".op4");
+
+debugger;
+
+opTabela1.addEventListener("click", ()=>{
+    opTabela1.style.backgroundColor = "#494981";
+    opTabela1.style.color = "#fff";
+    opTabela2.style.backgroundColor = "#000048";
+    opTabela2.style.color = "#fff";
+    opTabela3.style.backgroundColor = "#000048";
+    opTabela3.style.color = "#fff";
+    opTabela4.style.backgroundColor = "#000048";
+    opTabela4.style.color = "#fff";
+});
+opTabela2.addEventListener("click", ()=>{
+    opTabela1.style.backgroundColor = "#000048";
+    opTabela1.style.color = "#fff";
+    opTabela2.style.backgroundColor = "#494981";
+    opTabela2.style.color = "#fff";
+    opTabela3.style.backgroundColor = "#000048";
+    opTabela3.style.color = "#fff";
+    opTabela4.style.backgroundColor = "#000048";
+    opTabela4.style.color = "#fff";
+});
+opTabela3.addEventListener("click", ()=>{
+    opTabela1.style.backgroundColor = "#000048";
+    opTabela1.style.color = "#fff";
+    opTabela2.style.backgroundColor = "#000048";
+    opTabela2.style.color = "#fff";
+    opTabela3.style.backgroundColor = "#494981";
+    opTabela3.style.color = "#fff";
+    opTabela4.style.backgroundColor = "#000048";
+    opTabela4.style.color = "#fff";
+});
+opTabela4.addEventListener("click", ()=>{
+    opTabela1.style.backgroundColor = "#000048";
+    opTabela1.style.color = "#fff";
+    opTabela2.style.backgroundColor = "#000048";
+    opTabela2.style.color = "#fff";
+    opTabela3.style.backgroundColor = "#000048";
+    opTabela3.style.color = "#fff";
+    opTabela4.style.backgroundColor = "#494981";
+    opTabela4.style.color = "#fff";
+});
